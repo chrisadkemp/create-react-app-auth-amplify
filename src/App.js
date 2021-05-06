bd872db71d49441169282c2401638a801158a1dc
Christian Kemp <christian@fogldn.com>
2021-05-06 12:50:42 +0100

    added chat UI and material UI for amplify package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Index from './pages/index.js'
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
@@ -11,20 +11,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       
+        <Index></Index>
+        
+        
      
       </div>
     );
--- src/components/saySomethingBox/saySomethingBox.js
+++ src/components/saySomethingBox/saySomethingBox.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import TextField from '@material-ui/core/TextField';
-import SendIcon from '@material-ui/icons/Send';
+
 
 const useStyles = makeStyles(() => ({}));
 
@@ -18,7 +18,7 @@ const SaySomethingBox = (message) => {
       </Grid>
       <Grid xs={1} align="right">
         <Fab color="primary" aria-label="add">
-          <SendIcon />
+        
         </Fab>
       </Grid>
     </Grid>
